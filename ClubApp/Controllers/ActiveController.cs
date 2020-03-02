3ca46ee2d253347a78a84b4e0d00506a13a09ed3
王欢 <1335322586@qq.com>
2020-03-02 22:25:21 +0800

    增加活动管理页面

---
+++ ClubApp/Controllers/ActiveController.cs
@@ -0,0 +1,138 @@
+﻿using ClubApp.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClubApp.Controllers
+{
+    public class ActiveController : Controller
+    {
+        AppDbContext db = new AppDbContext();
+
+        private AppUserManager _userManager;
+
+        public ActiveController()
+        {
+        }
+
+        public ActiveController(AppUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public AppUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        // GET: Active
+        public ActionResult Index(string cid)
+        {
+            if (cid != null)
+            {
+                ClubNumber club = db.ClubNumbers.Find(cid);
+                if (club != null)
+                {
+                    ViewBag.ClubId = cid;
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Add(string cid)
+        {
+            if (cid == null)
+            {
+                return RedirectToAction("Index");
+            }
+            ClubNumber club = db.ClubNumbers.Find(cid);
+            if (club == null)
+            {
+                return HttpNotFound("未发现社团" + cid);
+            }
+            ViewBag.Club = club.Name;
+            return View();
+        }
+        public ActionResult Detail(string aid)
+        {
+            if (aid == null)
+            {
+                return RedirectToAction("Index");
+            }
+            if (!int.TryParse(aid, out int id))
+            {
+                return HttpNotFound("未发现活动" + aid);
+            }
+            Activities act = db.Activities.Find(id);
+            if (act == null)
+            {
+                return HttpNotFound("未发现活动" + aid);
+            }
+            return View(act);
+        }
+        [HttpGet]
+        public ActionResult AddAct(string cid)
+        {
+            if (cid == null)
+            {
+                return RedirectToAction("Index");
+            }
+            ClubNumber club = db.ClubNumbers.Find(cid);
+            if (club == null)
+            {
+                return HttpNotFound("未发现社团" + cid);
+            }
+            ActiveListModel model = new ActiveListModel();
+            model.ClubID = club.ClubId;
+            model.ClubName = club.Name;
+            return View(model);
+        }
+        [HttpPost,ValidateInput(false),Authorize]
+        public ActionResult AddAct(ActiveListModel model)
+        {
+            ClubNumber club = db.ClubNumbers.Find(model.ClubID);
+            UserNumber u = db.UserNumbers.Find(User.Identity.Name);
+            if (club == null)
+            {
+                return HttpNotFound("未发现社团" + model.ClubID);
+            }
+            Activities Act = new Activities()
+            {
+                Title1 = model.Title1,
+                Title2 = model.Title2,
+                Content = model.Content,
+                Club = club,
+                CreateDate = DateTime.Now,
+                User = u,
+                State = (int)ActiveState.已创建,
+                Votes0="0"
+            };
+            if (model.LabelStr.Length > 1)
+            {
+                Act.Label = model.LabelStr.Substring(1, model.LabelStr.Length - 1);
+            }
+            DateTime t1 ;
+            DateTime.TryParse(model.Time1, out t1);
+            Act.Time1 = t1;
+            DateTime.TryParse(model.Time2, out t1);
+            Act.Time2 = t1;
+            db.Activities.Add(Act);
+            db.SaveChanges();
+            return View("Index");
+        }
+        [HttpPost]
+        public ActionResult Add(ActiveListModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ ClubApp/Models/ActiveModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClubApp.Models
+{
+    public class ActiveModel
+    {
+
+    }
+    public class ActiveListModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "标签")]
+        public List<string> Labels { get; set; }
+        [Display(Name = "标签")]
+        public string LabelStr { get; set; }
+        [Display(Name ="头像")]
+        public string HeadImg { get; set; }
+        [Display(Name = "主标题"), MaxLength(100)]
+        public string Title1 { get; set; }
+        [Display(Name = "简介"), MaxLength(100)]
+        public string Title2 { get; set; }
+        [Display(Name = "内容"), MaxLength(1000)]
+        public string Content { get; set; }
+        [Display(Name = "活动图片")]
+        public List<string> ImgList { get; set; }
+        [Display(Name = "人数限制")]
+        public string MaxUser { get; set; }
+        [Display(Name = "活动地点")]
+        public string Area { get; set; }
+        [Display(Name = "开始时间")]
+        public string Time1 { get; set; }
+        [Display(Name = "结束时间")]
+        public string Time2 { get; set; }
+        [Display(Name = "发布时间")]
+        public string CreateDate { get; set; }
+        [Display(Name = "发布用户")]
+        public string UserID { get; set; }
+        [Display(Name = "发布用户")]
+        public string UserName { get; set; }
+        [Display(Name = "发布社团")]
+        public string ClubID { get; set; }
+        [Display(Name = "发布社团")]
+        public string ClubName { get; set; }
+        [Display(Name = "状态")]
+        public string State { get; set; }
+        [Display(Name = "评分")]
+        public string Votes0 { get; set; }
+        [Display(Name = "评论数量")]
+        public string VoteCount { get; set; }
+    }
+}
